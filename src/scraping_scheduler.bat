58cbef09cc09d163fd1097b072df8ac749c2dac9
francocontigo <ricardoaugustofranco@hotmail.com>
2023-11-21 17:39:47 -0300

    better instructions

--- src/scraping_scheduler.bat
+++ src/scraping_scheduler.bat
@@ -1,12 +1,12 @@
 @echo off
-REM Replace the path below with the actual path to scraping.py.
+REM Replace the path below with the actual absolute path to scraping.py.
 python C:...\wheater_report_scraping\src\scraping.py
 REM --------------------
 
 REM Set Up Task Scheduler:
-REM a. Open the Task Scheduler on your Windows machine. You can do this by searching for "Task Scheduler" in the Start menu.
-REM b. In the Task Scheduler, click on "Create Basic Task..." in the right-hand Actions pane.
-REM c. Follow the wizard to set a name and description for your task.
-REM d. Choose the trigger "Daily" and set the start date and time to 12:00 PM.
-REM e. Set the action to "Start a program" and browse to the location where your batch file (run_python_script.bat) is located.
-REM f. Complete the wizard, review your settings, and click "Finish."
+REM 1. Open the Task Scheduler on your Windows machine. You can do this by searching for "Task Scheduler" in the Start menu.
+REM 2. In the Task Scheduler, click on "Create Basic Task..." in the right-hand Actions pane.
+REM 3. Follow the wizard to set a name and description for your task.
+REM 4. Choose the trigger "Daily" and set the start date and time to 12:00 PM.
+REM 5. Set the action to "Start a program" and browse to the location where your batch file (run_python_script.bat) is located.
+REM 6. Complete the wizard, review your settings, and click "Finish."
